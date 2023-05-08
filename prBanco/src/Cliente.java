1467266b1cc80394d1b5aadf1f0d2dbf73af6e8f
Nuzagama <alvarorc25@gmail.com>
2023-05-08 10:59:45 +0200

    Cliente Getters and Setters

--- prBanco/src/Cliente.java
+++ prBanco/src/Cliente.java
@@ -1,4 +1,25 @@
 
 public class Cliente {
-
+	
+	String nombre = "";
+	String apellidos = "";
+	
+	public static void ConstructorCliente (String nombre, String apellidos) {
+		
+	}
+	
+	public void setNombre (String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setApellidos (String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getNombre (String nombre) {
+		return nombre;
+	}
+	
+	public String getApellidos (String apellidos) {
+		return apellidos;
+	}
 }
