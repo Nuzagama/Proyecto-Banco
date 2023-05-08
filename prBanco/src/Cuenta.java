f65d015445f2eaa4582dd5debdf499c57f7b72de
Nuzagama <alvarorc25@gmail.com>
2023-05-08 11:09:34 +0200

    Cuenta.java Getters n Setters

---
+++ prBanco/src/Cuenta.java
@@ -0,0 +1,38 @@
+
+public class Cuenta {
+	
+	int iban;
+	double saldo;
+	String estado;
+
+	public void BancoDos (int iban, double saldo, String estado) {
+		this.iban = iban;
+		this.saldo = saldo;
+		this.estado = estado;
+		}
+	
+	public void setIban (int iban) {
+		this.iban = iban;
+	}
+	
+	public void setSaldo (double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public int getIban (int iban) {
+		return iban;
+	}
+	
+	public double getSaldo (double saldo) {
+		return saldo;
+	}
+	
+	public void setEstado (String estado) {
+		this.estado = estado;
+	}
+	
+	public String getEstado (String estado) {
+		return estado;
+	}
+	
+}
