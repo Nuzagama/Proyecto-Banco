20305e9a5d798fa4ac54b8b1072d38478e9b6cd2
Nuzagama <alvarorc25@gmail.com>
2023-05-08 10:52:39 +0200

    Banco Getters y Setters

--- prBanco/src/Banco.java
+++ prBanco/src/Banco.java
@@ -1,8 +1,26 @@
 
 public class Banco {
+	
+	int iban = 0;
+	double saldo = 0;
 
 	public static void BancoDos (int iban, double saldo) {
-		
+		}
+	
+	public void setIban (int iban) {
+		this.iban = iban;
+	}
+	
+	public void setSaldo (double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public int getIban (int iban) {
+		return iban;
+	}
+	
+	public double getSaldo (double saldo) {
+		return saldo;
 	}
 	
 }
